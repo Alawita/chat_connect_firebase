f3b056ca26a3cfa55f18a9a223bbe784f777e6c0
Alawita <alybinrajab@gmail.com>
2024-01-11 12:45:03 +0300

    finished pulling users from firebase

--- chat_connect_app/lib/app/modules/navibar/pages/profile_screen.dart
+++ chat_connect_app/lib/app/modules/navibar/pages/profile_screen.dart
@@ -1,15 +1,35 @@
+import 'package:chat_connect_app/app/config/routes/my_named_routes.dart';
+import 'package:chat_connect_app/app/modules/auth/domain/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      color: Colors.green,
-      height: 300,
-      width: 300,
-    ));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authProvider = ref.read(authControllerProvider.notifier);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+            onPressed: () {
+              authProvider.signOut().then((value) {
+                if (value == true) {
+                  context.goNamed(MyNamedRoutes.login);
+                }
+              });
+            },
+            child: Text("Logout")),
+        Center(
+            child: Container(
+          color: Colors.green,
+          height: 300,
+          width: 300,
+        )),
+      ],
+    );
   }
 }
